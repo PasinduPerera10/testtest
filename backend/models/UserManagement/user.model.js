09e55ee4d36e236df27f85a9b49174de67a66d0d
Pasindu Perera <89623553+PasinduPerera10@users.noreply.github.com>
2023-05-24 03:55:57 +0530

    commmit

---
+++ backend/models/UserManagement/user.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    // mobile: { type: String},
+    // dob: { type: Date},
+    // country: { type: String },
+    passwordHash: { type: String, required: true },
+    userType: { type: String },
+    verified: { type: Boolean, default: false },
+    adminCreated: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
